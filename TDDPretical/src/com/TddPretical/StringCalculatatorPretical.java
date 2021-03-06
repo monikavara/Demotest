89faa549b6bf8fd264ab40cf2017ad8fcdb24405
hp <hp@DESKTOP-MRMI2P9>
2021-03-06 23:28:49 +0530

    1.The method can take 0 numbers, and will return their sum and Generate Success and Faliure TestCase

---
+++ TDDPretical/src/com/TddPretical/StringCalculatatorPretical.java
@@ -0,0 +1,17 @@
+package com.TddPretical;
+
+public class StringCalculatatorPretical {
+
+	public static int add(String stringOfNumbers) {
+		if (stringOfNumbers.isEmpty())
+			return 0;		
+		String[] numbers = stringOfNumbers.split(",");
+		int sum = 0;
+		for (String eachNumber : numbers) {
+			sum += Integer.valueOf(eachNumber);
+		}
+
+		return sum;
+	}
+	
+}
---
+++ TDDPretical/test/com/TddPretical/StringCalculatorTestPretical.java
@@ -0,0 +1,59 @@
+package com.TddPretical;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringCalculatorTestPretical {
+
+	//1.The method can take 0, 1 or 2 numbers, and will return their sum
+	
+		//Success
+		
+		@Test
+		public void isZeroForAnEmptyStringSuccess() {
+			int result = StringCalculatatorPretical.add("");
+			assertEquals(0, result);
+		}
+		
+		
+		//Failure
+		
+		/*@Test
+		public void isZeroForAnEmptyStringFailure() {
+			int result = StringCalculatatorPretical.add("1");
+			assertEquals(0, result);
+		}*/
+		
+		//Success 
+		@Test
+		public void OnlyOneNumberSuccess() {
+			int result = StringCalculatatorPretical.add("1");
+			assertEquals(1, result);
+		}
+		
+		//Failure
+		/*@Test
+		public void OnlyOneNumberSuccessFaliure() {
+			int result = StringCalculatatorPretical.add("1");
+			assertEquals(1, result);
+		}*/
+		
+		
+		//Success
+		@Test
+		public void sumsTheTwoNumbersSccuess() {
+			int result = StringCalculatatorPretical.add("1,2");
+			assertEquals(3, result);
+		}
+		
+		//Failure
+		@Test
+		public void sumsTheTwoNumbersFaliure() {
+			int result = StringCalculatatorPretical.add("1,2");
+			assertEquals(1, result);
+		}
+	
+	
+	
+}
